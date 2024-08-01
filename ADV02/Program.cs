29004e945424e432d4992e5927aa327277e187c6
Kerollos90 <emadkero318@gmail.com>
2024-08-01 20:28:45 +0300

    Add project files.

---
+++ ADV02/Program.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+
+internal class Program
+{
+
+    
+    public static void ArraylistRevere(ArrayList arrays ) 
+    {
+        
+        int left = 0;
+        int right = arrays.Count -1;
+
+        
+      
+        
+            while (left  <right)
+            {
+                object  temp = arrays[left];
+                arrays[left] = arrays[right];
+                arrays[right] = temp;
+
+                ++left;
+                --right;
+
+            }
+       
+        
+        
+
+
+
+
+    
+    
+    
+    
+    
+    
+    }
+
+    public static List<int> listEvenNum(List<int> values)
+    {
+        List<int> num = new List<int>();
+        foreach (int value in values)
+            if(value %2 ==0)
+            num.Add(value);
+        return num;
+
+    }
+
+    public  class FixedSizeList<T>
+    {
+        public T[] Value{ get; set; }
+        public int Count { get; set; }
+
+        
+
+
+
+        public FixedSizeList(int capacity)
+        {
+            if (capacity <= 0)
+            {
+                throw new ArgumentException("Capacity must be greater than zero.");
+            }
+            Value = new T[capacity];
+            
+
+
+
+        }
+
+        public void Add(T value)
+        {
+            if (Count >= Value.Length)
+            {
+                throw new InvalidOperationException("List is full.");
+            }
+            Value[Count++] = value;
+            
+        }
+        public T Get(int index)
+        {
+            if ( index >= Count)
+            {
+                throw new ArgumentOutOfRangeException($"{index} Index is out of range.");
+            }
+            return Value[index];
+        }
+
+
+    }
+
+    public static void Main(string[] args)
+    {
+
+
+        #region Q1
+
+        //ArrayList arrayLists = new ArrayList() { 1,2,3,4,5,6,7,8,9,14,15,16,17,18,19,20};
+
+        //ArraylistRevere(arrayLists);
+
+        //foreach (int i in arrayLists)
+        //Console.Write(i +" "); 
+        #endregion
+
+
+        #region Q2
+
+        //List<int> list = new List<int>()  { 1,2,3,4,5,6,7,8,9,10,11,12,13};        
+        //foreach (int i in listEvenNum(list))
+        //    Console.Write(i+" ,"); 
+        #endregion
+
+
+
+
+
+        #region Q3
+        //var list = new FixedSizeList<int>(3);
+        //list.Add(1);
+        //list.Add(2);
+        //list.Add(3);
+
+
+        //Console.WriteLine($"{list.Count} , {list.Value}");
+        //Console.WriteLine(list.Get(2)); 
+        #endregion
+
+
+
+
+
+
+
+
+    }
+}
---
+++ DEMO/Program.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections;
+
+internal class Program
+{
+
+
+    public static int SumArrayList(ArrayList arrayList)
+    {
+
+        int sum = 0;
+        if (arrayList is not  null)        
+            for (int i = 0; i < arrayList.Count; i++)
+                sum += (int)arrayList[i];
+        return sum;
+    
+    
+    
+    }
+
+    private static void Main(string[] args)
+    {
+        #region Non Generic Collection
+        //ArrayList arrayList = new ArrayList();
+        //Console.WriteLine($"Count of array liist = {arrayList.Count} , {arrayList.Capacity}");
+
+        //arrayList.Add( 1 );
+        //Console.WriteLine($"Count of array liist = {arrayList.Count} , {arrayList.Capacity}");
+
+        //arrayList.AddRange( new int[] { 3,4} );
+        //Console.WriteLine($"Count of array liist = {arrayList.Count} , {arrayList.Capacity}");
+
+        //arrayList.Add(5);
+        //Console.WriteLine($"Count of array liist = {arrayList.Count} , {arrayList.Capacity}");
+
+
+
+
+
+        //ArrayList arrayList = new ArrayList(5) { 1,2,3,4,5};
+        //Console.WriteLine($"Count of array liist = {arrayList.Count} , {arrayList.Capacity}");
+        //arrayList.Add(6);
+        //Console.WriteLine($"Count of array liist = {arrayList.Count} , {arrayList.Capacity}");
+        //arrayList.TrimToSize();
+        //Console.WriteLine($"Count of array liist = {arrayList.Count} , {arrayList.Capacity}");
+        //arrayList.Add(1);
+        ////arrayList.Add("kerollos");
+
+
+        //int  result = SumArrayList(arrayList);
+        //Console.WriteLine(result);
+
+        //foreach(int i in arrayList)
+        //Console.WriteLine(i); 
+        #endregion
+
+
+        #region Generic Collection
+        //List<int> list = new List<int>();
+        //Console.WriteLine($"Count of array list = {list.Count} , {list.Capacity}");
+        //list.Add(1);
+        //Console.WriteLine($"Count of array list = {list.Count} , {list.Capacity}");
+        //list.Add(2);
+        //list.AddRange(new int[] { 3, 4 });
+        //Console.WriteLine($"Count of array list = {list.Count} , {list.Capacity}");
+        //list.Add(5);
+        //Console.WriteLine($"Count of array list = {list.Count} , {list.Capacity}");
+        //list.TrimExcess();
+        //Console.WriteLine($"Count of array list = {list.Count} , {list.Capacity}");
+
+
+
+        //List<int> list = new List<int>(5) {1,2,3,4,5 };
+        ////Console.WriteLine($"Count of array list = {list.Count} , {list.Capacity}");
+        //list.Add(6);
+        ////Console.WriteLine($"Count of array list = {list.Count} , {list.Capacity}");
+
+        //foreach (int i in list) 
+        //    Console.Write(  i+" ,");
+        //Console.WriteLine();
+        //list[3] = 45;
+        //for (int i = 0;i < list.Count;i++)
+        //    Console.Write(list [i] + " ," );
+
+        #endregion
+
+
+        #region List Methods
+        //List<int> list = new List<int>(4) { 1,2,3,4};
+        //Console.WriteLine($"Count of array list = {list.Count} , {list.Capacity}");
+        //list.Add(5);
+        //list.AddRange(new int[] { 6, 7 });
+        //Console.WriteLine($"Count of array list = {list.Count} , {list.Capacity}");
+        //foreach (int i in list) Console.Write(  i +" ,");
+        //Console.WriteLine();
+        //list.Insert(6, 2);
+        //foreach (int i in list) 
+        //    Console.Write(  i +" ,");  
+        //list.InsertRange(4,new int[] { 9,50});
+        //Console.WriteLine();
+        //Console.WriteLine($"Count of array list = {list.Count} , {list.Capacity}");
+        //foreach (int i in list) 
+        //    Console.Write(  i +" ,");
+        //Console.WriteLine();
+
+        //int index = list.BinarySearch(90);
+        //Console.WriteLine(index);
+        //Console.WriteLine();
+        //list.Clear();
+        //Console.WriteLine($"Count of array list = {list.Count} , {list.Capacity}");
+        //Console.WriteLine(list.Contains(9));
+
+        //List<int> list = new List<int>(5) { 1, 2, 3, 4 };
+        //int[] ints = new int[10];
+        //list.CopyTo(ints);
+        //list.CopyTo(ints, 3);
+        //list.CopyTo(1,ints,5,3);
+        //foreach (int i in ints)
+        //    Console.Write((i)+" ,");
+        //list.EnsureCapacity(15);
+        //Console.WriteLine($"Count of array list = {list.Count} , {list.Capacity}");
+
+
+        //List<int> list = new List<int>(10) { 1, 2, 3, 4 ,56,6,4,8,45};
+        //int index = list.IndexOf(3);
+        //Console.WriteLine(index);
+        //
+        //list.Remove(4);
+        //foreach (int i in list)
+        //    Console.WriteLine(i); 
+        #endregion
+
+        #region Linked List
+        //LinkedList<int> ints = new LinkedList<int>();
+        //Console.WriteLine(ints.Count);
+
+        //ints.AddFirst(1);
+        //Console.WriteLine(ints.Count);
+
+        //ints.AddLast(1);
+        //Console.WriteLine(ints.Count);
+
+        //LinkedListNode<int> ints1 = ints.Find(1);
+        //ints.AddAfter(ints1, 1);
+        //Console.WriteLine(ints.Count); 
+        #endregion
+
+
+
+
+        #region Stack
+        //Stack<int> stack = new Stack<int>();
+        //stack.Push(1);
+        //stack.Push(2);
+        //stack.Push(3);
+
+        //Console.WriteLine(stack.Peek());
+
+        //Console.WriteLine();
+        //foreach (int i in stack)
+        //    Console.WriteLine(i);
+
+
+        //Console.WriteLine();
+        //Console.WriteLine(stack.Pop());
+        //foreach (int i in stack)
+        //    Console.WriteLine(i);
+
+
+
+        //bool result = stack.TryPop(out int Element);
+
+        //Console.WriteLine(result);
+        //Console.WriteLine(Element); 
+        #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
